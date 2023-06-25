3673ffe99b479ecb40b8241c2e7754b611421d67
lchang751 <136843170+lchang751@users.noreply.github.com>
2023-06-24 22:16:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ def main():
     ##################################################
     males=40
     females=60
-    m_perc=males/100
-    f_perc=females/100
+    m_perc=males
+    f_perc=females
     total=males+females
 
     print(total)
     print(males)
     print(females)
-    print (f’Percent males \t {m_perc:.2f}’)
-    print (f’Percent females \t {f_perc:.2f}’)
+    print (f'Percent males \t {m_perc:.2f}')
+    print (f'Percent females \t {f_perc:.2f}')
     ##################################################
     """
 

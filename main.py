d2714419f044d6ba89f0c61c7ca3bca672fc8a54
lchang751 <136843170+lchang751@users.noreply.github.com>
2023-06-24 22:37:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def main():
     """
     ##################################################
-    males=40
-    females=60
+    # Comlete your code here
+    Use the same variables: m_perc f_perc
+    ##################################################
+    """
+    males=int(input("Enter number of males: "))
+    females=int(input("Enter number of females: "))
     total=males+females
     m_perc=(males/total)*100
     f_perc=(females/total)*100
@@ -12,10 +16,6 @@ def main():
     print(females)
     print (f'Percent males \t {m_perc:.2f}')
     print (f'Percent females \t {f_perc:.2f}')
-
-    ##################################################
-    """
-
     """
     ########################################
     # Do not delete the return statement
